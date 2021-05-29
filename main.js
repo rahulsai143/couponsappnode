70ce63a27ae2af59c442f22b0daf7bfdaf54c6a4
Rahul Reddy <kandularahulreddy96@gmail.com>
2021-05-29 21:00:36 +0530

    [NoTicket] - Adding the travis.yml for travis build

--- main.js
+++ main.js
@@ -1,50 +1,64 @@
 /**
-*    module dependencies
-*
-*/
-const express = require('express');
-const bodyparser = require('body-parser');
-const cors = require('cors');
-const mongoose = require('mongoose');
+ *    module dependencies
+ *
+ */
+const express = require("express");
+const bodyparser = require("body-parser");
+const cors = require("cors");
+const mongoose = require("mongoose");
 
-const couponrouter = require('./couponsrouter/couponsrouter');
-const normalauth = require('./normalauthentication/normalauth');
+const couponrouter = require("./couponsrouter/couponsrouter");
+const normalauth = require("./normalauthentication/normalauth");
 
 const app = express();
 
 var db = null;
 
 app.use(bodyparser.json());
-app.use(cors({
-  'Access-Control-Allow-Origin': '*',
-  'Access-Control-Request-Headers': 'sessionId,Cache-Control, Pragma, Origin, Authorization, Content-Type, X-Requested-With',
-  'exposedHeaders': ['sessionId'],
-  'Access-Control-Allow-Methods': ['GET','HEAD','PUT','PATCH','POST','DELETE'],
-  'preflightContinue': false
-}));
-app.use('/couponrouter',couponrouter);
-app.use('/normalauth',normalauth);
+app.use(
+  cors({
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Request-Headers":
+      "sessionId, Cache-Control, Pragma, Origin, Authorization, Content-Type, X-Requested-With",
+    exposedHeaders: ["sessionId"],
+    "Access-Control-Allow-Methods": [
+      "GET",
+      "HEAD",
+      "PUT",
+      "PATCH",
+      "POST",
+      "DELETE",
+    ],
+    preflightContinue: false,
+  })
+);
+app.use("/couponrouter", couponrouter);
+app.use("/normalauth", normalauth);
 
-app.get('/',(req,res) =>{
-    res.writeHead(200,{'Content-Type':'text/plain'});
-    res.end('Main Html Page');
-})
+app.get("/", (req, res) => {
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end("Main Html Page");
+});
 
 // Initialize connection once
 //mongodb+srv://rahulsai143:rsns.kp143@couponsapp.zg3kl.mongodb.net/couponsapp?retryWrites=true&w=majority
-mongoose.connect('mongodb://127.0.0.1:27017/couponsapp', {
+mongoose.connect(
+  "mongodb://127.0.0.1:27017/couponsapp",
+  {
     keepAlive: true,
     socketTimeoutMS: 0,
-    useNewUrlParser:true,
+    useNewUrlParser: true,
     useCreateIndex: true,
-    useUnifiedTopology:true,
-    poolSize: 10
-},(err, database) => {
-  if(err) throw err;
-  db = database;
-  // Start the application after the database connection is ready
-  app.listen(3000);
-    console.log('the server is up and running');
-});
+    useUnifiedTopology: true,
+    poolSize: 10,
+  },
+  (err, database) => {
+    if (err) throw err;
+    db = database;
+    // Start the application after the database connection is ready
+    app.listen(3000);
+    console.log("the server is up and running");
+  }
+);
 
 module.exports = db;
